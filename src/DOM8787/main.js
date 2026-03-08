0bf20ce36c3735570def0481d82759bbdbab9371
shaman2016scratch <g.ilkh@yandex.ru>
2026-03-08 23:37:38 +0300

    upd

--- src/DOM8787/main.js
+++ src/DOM8787/main.js
@@ -1,1 +1,16 @@
+const DOM8787 = {
+    'metadata': {
+        'version': '0.0.1',
+        'versionNum': 1
+    },
+    'create': async (d) => {
+        return {
+            'content': d.innerHTML,
+            'update': function (c) {
+                d.innerHTML = c
+            }
+        }
+    }
+}
 
+export { DOM8787 }
