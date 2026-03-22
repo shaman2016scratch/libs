a0a3ece191b0ccee0c1e95baf0297fd13600f8a8
shaman2016scratch <g.ilkh@yandex.ru>
2026-03-22 10:28:42 +0300

    UPDATE DATAPACK

--- src/dataPack/index.js
+++ src/dataPack/index.js
@@ -18,11 +18,33 @@ const dataPack = {
                 },
                 jsonData?.data || {},
             ]
-        }
+        },
+        '3dBlock2': function(jsonData) {
+            return `
+                |X:${jsonData?.element?.x || null}|Y:${jsonData?.element?.y || null}|Z:${jsonData?.element?.z || null}|A:${jsonData?.element?.a || null}|B:${jsonData?.element?.b || null}|C:${jsonData?.element?.c || null};
+                3D DATA;
+                1.0.0;
+                2;
+                |name:${jsonData?.meta?.name || null};
+                ${jsonData?.data || {}}
+            `
+        },
+        'htmlobject': async function(jsonData) {
+            let ret = `
+              <html>
+            `
+            for(let i = 0; i < jsonData.length; i++) {}
+            ret += `
+              </html>
+            `
+        },
     },
     '3dBlock': {
-        '': '<class 3dBlock {...}>'
-    }
+        '': '<DATAPACK CLASS "3dBlock" {...}>'
+    },
+    '3dBlock2': {
+        '': '<DATAPACK CLASS "3dBlock2" {...}>'
+    },
 }
 
 export { dataPack }
